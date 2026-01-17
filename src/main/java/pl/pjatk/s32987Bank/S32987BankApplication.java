3dc8fea5430a824ca67ac20e53f6fdd31c595487
programKernel <s32987@pjwstk.edu.pl>
2026-01-17 15:19:13 +0100

    Initial commit

---
+++ src/main/java/pl/pjatk/s32987Bank/S32987BankApplication.java
@@ -0,0 +1,13 @@
+package pl.pjatk.s32987Bank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class S32987BankApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(S32987BankApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pl/pjatk/s32987Bank/S32987BankApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.pjatk.s32987Bank;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class S32987BankApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
